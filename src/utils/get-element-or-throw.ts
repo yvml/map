27de86f70f6be638a22589408ef48b55338866e8
garrepi <jacgarrett18@gmail.com>
2026-02-25 15:23:14 -0800

    cleanup

--- src/utils/get-element-or-throw.ts
+++ src/utils/get-element-or-throw.ts
@@ -3,20 +3,12 @@
  *
  * @throws if not found
  */
-export const getElementOrThrow = <T extends HTMLElement>(
-    element: { id: string } | { class: string },
-): T => {
-    if ("id" in element) {
-        const foundElement = document.getElementById(element.id);
-        if (!foundElement) {
-            throw Error("Element not found by id: " + element.id);
-        }
-        return foundElement as T;
-    } else {
-        const foundElement = document.getElementById(element.class);
-        if (!foundElement) {
-            throw Error("Element not found by class: " + element.class);
-        }
-        return foundElement as T;
+export const getElementOrThrow = <T extends HTMLElement>(element: {
+    id: string;
+}): T => {
+    const foundElement = document.getElementById(element.id);
+    if (!foundElement) {
+        throw Error("Element not found by id: " + element.id);
     }
+    return foundElement as T;
 };
